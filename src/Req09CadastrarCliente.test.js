066590af4778404ed40b3d696210f69e1e11a8f2
almeida1 <engsoftware1@gmail.com>
2025-03-13 17:55:04 -0300

    refatoracao dos testes de Req09CadastrarCliente 	modified:   src/Req09CadastrarCliente.test.js

--- src/Req09CadastrarCliente.test.js
+++ src/Req09CadastrarCliente.test.js
@@ -8,29 +8,3 @@ test("ct01 - verifica se titulo esta na pagina", () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test("ct02 - verifica a entrada de dados", async () => {
-  render(<App />);
-  const cpfInput = screen.getByTestId("cpf");
-
-  await userEvent.type(cpfInput, "123.456.789-00");
-
-  expect(cpfInput).toHaveValue("123.456.789-00");
-});
-
-
-test("ct03 - verifica se a validação impede envio de campos vazios", async () => {
-  render(<App />);
-  
-  const submitButton = screen.getByRole("button", { name: /confirmar/i });
-
-  // Simula clique no botão
-  await userEvent.click(submitButton);
-
-  const cpfInput = screen.getByTestId("cpf");
-  const nomeInput = screen.getByTestId("nome");
-
-  // Verifica se o campo está marcado como inválido
-  expect(cpfInput.validity.valueMissing).toBe(true);
-  expect(nomeInput.validity.valueMissing).toBe(true);
-});
-
